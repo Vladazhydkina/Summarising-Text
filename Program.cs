6c52ed35eec2cb13a3e5e60b2d367f109ba07a15
Vladazhydkina <114068921+Vladazhydkina@users.noreply.github.com>
2023-02-14 12:51:56 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace summarisingText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sentence = "This is going to be a really really really really really long text.";
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
+        }  
+    }
+}
